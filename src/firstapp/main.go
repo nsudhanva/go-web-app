93a0a04b922ebb1dad4f3b3d16a121181a63d970
Sudhanva Narayana <nsudhanva@gmail.com>
2020-04-05 18:23:28 +0530

    Hello World Web App

---
+++ src/firstapp/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello Go!"))
+	})
+
+	http.ListenAndServe(":8000", nil)
+}
